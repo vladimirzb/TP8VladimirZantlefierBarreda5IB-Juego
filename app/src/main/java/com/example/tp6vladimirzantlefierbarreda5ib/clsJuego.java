513124f2264a5f5c7a743500c069ad76305812c6
vladimirzb <55926702+vladimirzb@users.noreply.github.com>
2020-09-26 20:54:23 -0300

    Escenas implementadas (PDF 2)

--- app/src/main/java/com/example/tp6vladimirzantlefierbarreda5ib/clsJuego.java
+++ app/src/main/java/com/example/tp6vladimirzantlefierbarreda5ib/clsJuego.java
@@ -2,7 +2,9 @@ package com.example.tp6vladimirzantlefierbarreda5ib;
 
 import android.util.Log;
 
+import org.cocos2d.layers.Layer;
 import org.cocos2d.nodes.Director;
+import org.cocos2d.nodes.Scene;
 import org.cocos2d.opengl.CCGLSurfaceView;
 
 public class clsJuego {
@@ -19,5 +21,33 @@ public class clsJuego {
         Log.d("Comenzar", "Comienza el juego");
 
         Director.sharedDirector().attachInView(_VistaDelJuego);
+
+        Log.d("Comenzar", "Declaro e instancio la escena");
+        Scene escenaAUsar;
+        escenaAUsar = EscenaComienzo();
+
+        Log.d("Comenzar", "Le digo al director de mi jueguito que inicie la escena");
+        Director.sharedDirector().runWithScene(escenaAUsar);
+    }
+
+    private Scene EscenaComienzo()
+    {
+        Log.d("EscenaComienzo","comienza");
+        Scene escenaADevovler;
+        escenaADevovler=Scene.node();
+
+        Log.d("EscenaComienzo","Agrego capa");
+        capaJuego unaCapa;
+        unaCapa = new capaJuego();
+        escenaADevovler.addChild(unaCapa);
+
+        Log.d("EscenaComienzo","Devuelvo la escena creada");
+        return escenaADevovler;
+
+    }
+
+    class capaJuego extends Layer{
+
+
     }
 }
